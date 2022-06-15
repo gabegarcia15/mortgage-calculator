3f937463b7c1b9aa1037df0208ed9a747a5625e4
Gabe Garcia <gabe.garcia@devoted.com>
2022-06-15 10:09:23 -0400

    cleans up .py file

--- mortgage_payments.py
+++ mortgage_payments.py
@@ -1,11 +1,6 @@
-#print("Welcome to Mortgage Payments!")
-print("Are you considering buying a home? (Y/N")
-
 from art import *
-tprint("Welcome to Mortgage Payments!")
 
-tprint('''Lorem  
-ipsum 
-dolor''', font="big") # Multi-line print
+tprint("Mortgage Payments", font="charl")
 
-tprint("Mortgage Payments", font="charl") # Multi-line print
+# First Question
+is_buying_home = input("Are you considering buying a home? (Y/N): ")
