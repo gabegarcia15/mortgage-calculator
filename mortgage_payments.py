18b29b28fece544ac28bed19035ecf3fdf5d4a9f
Gabe Garcia <gabe.garcia@devoted.com>
2022-06-15 10:01:01 -0400

    tests display with art library

---
+++ mortgage_payments.py
@@ -0,0 +1,11 @@
+#print("Welcome to Mortgage Payments!")
+print("Are you considering buying a home? (Y/N")
+
+from art import *
+tprint("Welcome to Mortgage Payments!")
+
+tprint('''Lorem  
+ipsum 
+dolor''', font="big") # Multi-line print
+
+tprint("Mortgage Payments", font="charl") # Multi-line print
