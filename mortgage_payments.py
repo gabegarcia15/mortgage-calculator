dad3721b25fc946c7d19fe7e85bfa2db7ea8c530
Gabe Garcia <gabe.garcia@devoted.com>
2022-06-15 19:20:03 -0400

    input fix

--- mortgage_payments.py
+++ mortgage_payments.py
@@ -4,3 +4,16 @@ tprint("Mortgage Payments", font="charl")
 
 # First Question
 is_buying_home = input("Are you considering buying a home? (Y/N): ")
+
+buyer = True
+
+while buyer == True:
+    if(is_buying_home.upper())=="Y":
+        buyer = False
+        print("Good Luck!")
+    elif(is_buying_home.upper())=="N":
+        buyer = False
+        print("Too Bad!")
+    else:
+        print("That's not a valid response. Try again!")
+        is_buying_home = input("Are you considering buying a home? (Y/N): ")
