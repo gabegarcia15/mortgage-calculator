e5fd3dda755281b4f42e7c984640e80544932d57
Gabe Garcia <gabe.garcia@devoted.com>
2022-06-15 21:33:56 -0400

    creates is_buyer function

--- mortgage_payments.py
+++ mortgage_payments.py
@@ -2,18 +2,22 @@ from art import *
 
 tprint("Mortgage Payments", font="charl")
 
-# First Question
-is_buying_home = input("Are you considering buying a home? (Y/N): ")
 
-buyer = True
 
-while buyer == True:
-    if(is_buying_home.upper())=="Y":
-        buyer = False
-        print("Good Luck!")
-    elif(is_buying_home.upper())=="N":
-        buyer = False
-        print("Too Bad!")
-    else:
-        print("That's not a valid response. Try again!")
-        is_buying_home = input("Are you considering buying a home? (Y/N): ")
+def is_buyer():
+    # Input
+    is_buying_home = input("Are you considering buying a home? (Y/N): ")
+    buyer = True
+
+    while buyer == True:
+        if(is_buying_home.upper())=="Y":
+            buyer = False
+            print("Good Luck!")
+        elif(is_buying_home.upper())=="N":
+            buyer = False
+            print("Too Bad!")
+        else:
+            print("That's not a valid response. Try again!")
+            is_buying_home = input("Are you considering buying a home? (Y/N): ")
+
+is_buyer()        
