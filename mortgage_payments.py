fea61d692e64211277aab51c4f30c8b44cc56f78
Gabe Garcia <gabe.garcia@devoted.com>
2022-06-15 23:14:32 -0400

    rm comments

--- mortgage_payments.py
+++ mortgage_payments.py
@@ -34,7 +34,6 @@ loan_term_payments = loan_term*12
 
 first_numerator = (1+monthly_interest_rate)**loan_term_payments
 second_numerator =  monthly_interest_rate*first_numerator
-#numerator = loan_amount*((1+monthly_interest_rate)**loan_term_payments)
 denominator = ((1+monthly_interest_rate)**loan_term_payments)-1
 mortgage_payment = loan_amount*(second_numerator/denominator)
 
